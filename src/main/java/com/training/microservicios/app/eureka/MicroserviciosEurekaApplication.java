243d3ca4d3d6eb95a8f0b4781294e9c48cc96a50
Josue Bautista Pérez <josue.bautista@ferromex.mx>
2023-08-31 17:37:56 -0600

    Fase 1

--- src/main/java/com/training/microservicios/app/eureka/MicroserviciosEurekaApplication.java
+++ src/main/java/com/training/microservicios/app/eureka/MicroserviciosEurekaApplication.java
@@ -2,7 +2,11 @@ package com.training.microservicios.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+
+@EnableEurekaServer //habilita eureka server
 @SpringBootApplication
 public class MicroserviciosEurekaApplication {
 
